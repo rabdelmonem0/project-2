9fa1b0e05828029a69e05fb446a9bbd1392e6ee4
Rwan <rabdelmonem0@gmail.com>
2021-05-08 14:11:03 -0700

    worked on user model and login.js

---
+++ models/Post-RA.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    post_text: {
+        type: DataTypes.TEXT,
+        allowNull: fasle
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+},
+{
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: post
+});
+
+module.exports = Post;
