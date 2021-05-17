c7c23a3ccc4692111102f7db4a9d5267584a49e5
Derrick Helgeson <dkmk1020@gmail.com>
2021-05-17 08:44:47 -0700

    test

--- models/home.js
+++ models/home.js
@@ -2,42 +2,51 @@ const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 const express = require('express');
 const fileUpload = require('express-fileupload');
-
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.use(fileUpload());
-
-//template engine
-app.engine('hbs', exphbs({ exname: '.hbs'}));
-app.set('view engine', 'hbs');
+console.log('hello');
+// const router = require('express').Router();
 
 
-app.get('', (req, res) => {
-    res.render('index')
-});
+// const app = express();
+// const port = process.env.PORT || 3001;
+
+// app.use(fileUpload());
+
+// //template engine
+// app.engine('hbs', exphbs({ exname: '.hbs'}));
+// app.set('view engine', 'hbs');
 
 
-app.post('', (req, res) => {
-    let person;
-    let fileUploadPath;
-
-    if(!req,files || Object.keys(req,files).length === 0){
-        return res.status(400).send('No files were uploaded.');
-    }
-
-    person = req.files.person;
-    fileUploadPath =__dirname + '/upload/' + person.name;
-
-    console.log(person);
-
-    person.mv(fileUploadPath, function(err) {
-        if(err) return res.status(500).send(err);
-
-        res.send('File uploaded!');
-
-    });
-});
+// app.get('', (req, res) => {
+//     res.render('index')
+// });
 
 
-app.listen(port, ()=> console.log(`listening on port ${port}`));
+// router.get('/', (req, res) => {
+//     res.render('homepage')
+// });
+
+// module.exports = router;
+
+// app.post('', (req, res) => {
+//     let person;
+//     let fileUploadPath;
+
+//     if(!req,files || Object.keys(req,files).length === 0){
+//         return res.status(400).send('No files were uploaded.');
+//     }
+
+//     person = req.files.person;
+//     fileUploadPath =__dirname + '/upload/' + person.name;
+
+//     console.log(person);
+
+//     person.mv(fileUploadPath, function(err) {
+//         if(err) return res.status(500).send(err);
+
+//         res.send('File uploaded!');
+
+//     });
+// });
+
+
+// app.listen(port, ()=> console.log(`listening on port ${port}`));
